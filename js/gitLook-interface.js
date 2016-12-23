e1f09d648c59dffc3133ec9772938b404f37983a
zacharybaum42@gmail.com <zacharybaum42@gmail.com@>
2016-12-23 10:51:22 -0500

    add repositories to response

--- js/gitLook-interface.js
+++ js/gitLook-interface.js
@@ -8,11 +8,17 @@ $(document).ready(function() {
     $('#username').val("");
     console.log('https://api.github.com/users/' + user + '?access_token=' + apiKey);
 
-    $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
-      console.log(JSON.stringify(response));
+    $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
       $('.showUsers').text("");
-      $('.showUsers').append("<h2>" + response.login + "</h2>");
-      $('.showUsers').append("<a href=" + response.html_url + "><p>Link</p></a>");
+      console.log(JSON.stringify(response));
+
+      $('.showUsers').append("<h2>" + response[0].owner.login + "</h2>");
+      $('.showUsers').append("<a href=" + response[0].owner.html_url + "><p>Link</p></a>");
+
+      for (repository of response) {
+        $('.showUsers').append("<h4>" + repository.name + "</h4>");
+      }
+
     }).fail(function(error) {
       $('.showUsers').text(error.responseJSON.message);
     });
